9d7d083c93605362fdba21a55a0aa6bb8f4eb586
Joseph Won <godspeedyoo@gmail.com>
2015-04-06 16:48:30 -0700

    Add #show and #create actions to GridController

--- app/controllers/grid_controller.rb
+++ app/controllers/grid_controller.rb
@@ -1,3 +1,24 @@
 class GridController < ApplicationController
 
+  def show
+    @grid = Grid.find(params[:size])
+  end
+
+  def create
+    @grid = Grid.find_or_create_by(grid_params)
+
+    if @grid.save
+      redirect_to @grid
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def grid_params
+    params.require(:grid).permit(:size)
+  end
+
 end
--- db/migrate/20150406232132_create_grids.rb
+++ db/migrate/20150406232132_create_grids.rb
@@ -1,6 +1,7 @@
 class CreateGrids < ActiveRecord::Migration
   def change
     create_table :grids do |t|
+      t.integer :size
 
       t.timestamps null: false
     end
