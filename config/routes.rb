aa4c4198fe1a453722ccbb4e79a6490af3f88440
Joseph Won <godspeedyoo@gmail.com>
2015-04-06 16:41:57 -0700

    Add grid and nested squares as resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   
+  resources :grids do
+    resources :squares
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
