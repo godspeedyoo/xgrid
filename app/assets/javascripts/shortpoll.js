9e5049adcdab2e3d7aa4d23b59b6ecd0cbc18e75
Joseph Won <godspeedyoo@gmail.com>
2015-04-07 15:41:18 -0700

    Implement DOM manipulation of each cell based on on response of poll - grid state is now synced across browsers/clients

--- app/assets/javascripts/shortpoll.js
+++ app/assets/javascripts/shortpoll.js
@@ -1,17 +1,18 @@
 (function(document) {
+	// grabbing grid ID from URL to avoid waiting on DOM load
 	var current_url = window.location.href.split('/');
 	var gridId = parseInt(current_url[current_url.length - 1]);
 
-  var POLL_URL = 'localhost:3000/grids/data/',
+  var POLL_URL = 'http://localhost:3000/grids/data/',
       POLL_FREQUENCY = 2000;  // every second
 
 	function getSquaresData(gridId) {
-		console.log("Retrieving squares data...");
 		var request = new XMLHttpRequest();
 
 		request.onreadystatechange = function() {
 			if (request.readyState == 4 && request.status == 200) {
-				console.log(request.responseText);
+				var data = JSON.parse(JSON.parse(request.responseText)['data']);
+				updateCells(data);
 			}
 		}
 
@@ -21,7 +22,34 @@
 		request.send();
 	}
 
-	setInterval(function() {getSquaresData(gridId)}, POLL_FREQUENCY); 
+	setInterval(function() { getSquaresData(gridId) }, POLL_FREQUENCY); 
+
+	function updateCells(data) {
+		console.log(data);
+		for (var i = 0; i < data.length; i++) {
+			var currentCell = document.querySelector("[data-square-index='" + i + "']");
+			var squareStateOfDOM = currentCell.dataset.squareState;
+
+			if (data[i] != squareStateOfDOM) {
+				console.log("attempting change");
+				updateCell(currentCell, data[i]);
+			} 
+		}
+	}
+
+	function updateCell(cell, squareState) {
+		if (cell != null) {
+			if (squareState == 1) {
+				cell.className = cell.className + " x"; 
+				cell.dataset.squareState = 1;
+				cell.setAttribute('draggable', true);
+			} else {
+				cell.className = 'cell'
+				cell.dataset.squareState = 0;
+				cell.setAttribute('draggable', false);
+			}
+		}
+	}
 
 
 
