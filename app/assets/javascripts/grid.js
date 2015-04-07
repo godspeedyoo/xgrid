9c6f74f86aa655a6a1fed2c6dfb8bcb59ac39593
Joseph Won <godspeedyoo@gmail.com>
2015-04-06 21:47:50 -0700

    Add event listener to grid and console log squareIndex

--- app/assets/javascripts/grid.js
+++ app/assets/javascripts/grid.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+window.onload = function() {
+	var grid = document.getElementsByClassName('grid-container')[0];
+
+	console.log(grid);
+	grid.addEventListener('click', function(e) {
+		console.log(e.target.dataset.squareIndex);
+	})
+}
