b8a9fc41da09760c96925560695a97f69e17eea1
Joseph Won <godspeedyoo@gmail.com>
2015-04-07 15:09:08 -0700

    Refactor to wrap polling function in IIFE; ajax request now reads correct square data and displays in console; need to implement DOM manipulation based on response data

--- app/assets/javascripts/shortpoll.js
+++ app/assets/javascripts/shortpoll.js
@@ -1,62 +1,28 @@
-(function() {
-	function getSquaresData(gridId) {
-	console.log("Retrieving squares data...");
-	var request = new XMLHttpRequest();
+(function(document) {
+	var current_url = window.location.href.split('/');
+	var gridId = parseInt(current_url[current_url.length - 1]);
 
-	request.onreadystatechange = function() {
-		if (request.readyState == 4 && request.status == 200) {
-			console.log(request.responseText);
+  var POLL_URL = 'localhost:3000/grids/data/',
+      POLL_FREQUENCY = 2000;  // every second
+
+	function getSquaresData(gridId) {
+		console.log("Retrieving squares data...");
+		var request = new XMLHttpRequest();
+
+		request.onreadystatechange = function() {
+			if (request.readyState == 4 && request.status == 200) {
+				console.log(request.responseText);
+			}
 		}
+
+		request.open('GET', POLL_URL + gridId, true);
+		request.withCredentials = true;
+		request.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+		request.send();
 	}
 
-	request.open('GET', 'http://localhost:3000/grids/data/' + gridId, true);
-	request.withCredentials = true;
-	request.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-	request.send();
-}
+	setInterval(function() {getSquaresData(gridId)}, POLL_FREQUENCY); 
 
 
 
-    var POLL_URL = 'localhost:3000/grids/',
-        POLL_FREQUENCY = 2000;  // every second
-
-	setInterval(function() {getSquaresData(3)}, 1000); 
-
-}());
-
-window.onLoad = function() {
-	var gridId = parseInt(document.getElementsByClassName('grid-container')[0].id);
-
-
-
-}
-
-	// 	request.onreadystatechange = function() {
-	// 		if (request.readyState == 4 && request.status == 200) {
-	// 			// returns data in format of Object {data: {array}, cellId: int}
-	// 			console.log(request.responseText);
-	// 			var data = JSON.parse(JSON.parse(request.responseText)['data']);
-
-	// 			cellToUpdate = document.querySelector("[data-square-index='" + cellId + "']");
-	// 			if (cellToUpdate != null) {
-	// 				if (data[cellId] === 1) {
-	// 					cellToUpdate.className = cellToUpdate.className + " x"; 
-	// 					cellToUpdate.dataset.squareState = 1;
-	// 					cellToUpdate.setAttribute('draggable', true);
-	// 				} else {
-	// 					cellToUpdate.className = 'cell'
-	// 					cellToUpdate.dataset.squareState = 0;
-	// 					cellToUpdate.setAttribute('draggable', false);
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-
-	// 	request.open('PUT', 'http://localhost:3000/grids/' + gridId, true);
-	// 	request.withCredentials = true;
-	// 	request.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-	// 	request.send("cellId=" + cellId);
-	// }
-    
-
-
+}(document));
