5b01ad1875cde169ccd17d5c7f4c8041dbc93572
Joseph Won <godspeedyoo@gmail.com>
2015-04-09 14:45:05 -0700

    Add preflight check method back to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   after_filter :set_cors
   skip_before_filter :verify_authenticity_token, :only => [:update]
+  before_filter :cors_preflight_check
 
 
   def set_cors
@@ -13,7 +14,6 @@ class ApplicationController < ActionController::Base
     headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With json auth_token X-CSRF-Token}.join(',')
     headers['Access-Control-Max-Age'] = '1728000'
   end
-  # before_filter :cors_preflight_check
   # after_filter :cors_set_access_control_headers
 
   # def cors_set_access_control_headers
@@ -24,14 +24,14 @@ class ApplicationController < ActionController::Base
   #   headers['Access-Control-Max-Age'] = '1728000'
   # end
 
-  # def cors_preflight_check
-  #   if request.method == 'OPTIONS'
-  #     headers['Access-Control-Allow-Origin'] = '*'
-  #     headers['Access-Control-Allow-Methods'] = 'POST','PUT','GET','OPTIONS'
-  #     headers['Access-Control-Request-Method'] = '*'
-  #     headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With json auth_token X-CSRF-Token}.join(',')
-  #     headers['Access-Control-Max-Age'] = '1728000'
-  #     render :text => '', :Content_type => 'text/plain'
-  #   end
-  # end
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST','PUT','GET','OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With json auth_token X-CSRF-Token}.join(',')
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :Content_type => 'text/plain'
+    end
+  end
 end
