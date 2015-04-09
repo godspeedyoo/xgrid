69fe4065ff5cba1d7342c39a39f8fb94071f8a8f
Joseph Won <godspeedyoo@gmail.com>
2015-04-08 20:00:57 -0700

    Further cleanup shortpoll, add comments

--- app/assets/javascripts/shortpoll.js
+++ app/assets/javascripts/shortpoll.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	var grid = document.getElementsByClassName('grid-container')[0];
 	var gridId = parseInt(document.getElementsByClassName('grid-container')[0].id);
-  var POLL_URL = 'http://localhost:3000/grids/data/',
-      POLL_FREQUENCY = 2000;  // every second
+	var POLL_URL = 'http://localhost:3000/grids/data/',
+      POLL_FREQUENCY = 1000;  // every second
 
+  // AJAX call to obtain array of binary ints representing empty cell or 'x'
+  // Each index from response data represents corresponding data-square-index 
 	function getSquaresData(gridId) {
 		console.log("polling...");
 		var request = new XMLHttpRequest();
@@ -16,17 +18,18 @@ document.addEventListener('DOMContentLoaded', function() {
 			}
 		}
 
-		if (gridId != NaN) {
-			request.open('GET', POLL_URL + gridId, true);
-			request.withCredentials = true;
-			request.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-			request.send();
-		}
+		request.open('GET', POLL_URL + gridId, true);
+		request.withCredentials = true;
+		request.setRequestHeader('Content-Type',
+														 'application/x-www-form-urlencoded');
+		request.send();
 	}
 
 	function updateCells(data) {
 		for (var i = 0; i < data.length; i++) {
-			var currentCell = document.querySelector("[data-square-index='" + i + "']");
+			var currentCell = document.querySelector("[data-square-index='" 
+																								+ i 
+																								+ "']");
 			var squareStateOfDOM = currentCell.dataset.squareState;
 
 			if (data[i] != squareStateOfDOM) {
@@ -36,21 +39,26 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 	function updateCell(cell, squareState) {
-		console.log(cell);
-		console.log(squareState);
-
 		if (cell != null) {
 			if (squareState == 1) {
-				cell.className = cell.className + " x"; 
-				cell.dataset.squareState = 1;
-				cell.setAttribute('draggable', true);
+				markCell(cell, squareState)
 			} else if (cell != null){
-				cell.className = 'cell'
-				cell.dataset.squareState = 0;
-				cell.setAttribute('draggable', false);
+				unmarkCell(cell, squareState)
 			}
 		}
 	}
 
-	setInterval( function() { getSquaresData(gridId) }, POLL_FREQUENCY); 
+	function markCell(cell, squareState) {
+		cell.className = cell.className + " x"; // apply the css styling for 'marked' cells
+		cell.dataset.squareState = 1;	// set the state of the square to be 'marked'
+		cell.setAttribute('draggable', true); // allow 'marked' cells to be draggable
+	}
+
+	function unmarkCell(cell, squarestate) {
+		cell.className = 'cell'
+		cell.dataset.squareState = 0;
+		cell.setAttribute('draggable', false);
+	}
+
+	setInterval( function() { getSquaresData(gridId) }, POLL_FREQUENCY); // poll and update DOM as necessary
 });
