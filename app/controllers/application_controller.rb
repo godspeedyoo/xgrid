7658e736bcc8048daed91bfb3a3ec8bc8fdf5139
Joseph Won <godspeedyoo@gmail.com>
2015-04-09 15:31:50 -0700

    try removing preflight check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   after_action :set_cors
-  skip_before_filter :verify_authenticity_token, :only => [:update]
-  before_filter :cors_preflight_check
+  # skip_before_filter :verify_authenticity_token, :only => [:update]
+  # before_filter :cors_preflight_check
 
 
   def set_cors
@@ -24,14 +24,14 @@ class ApplicationController < ActionController::Base
   #   headers['Access-Control-Max-Age'] = '1728000'
   # end
 
-  def cors_preflight_check
-    if request.method == 'OPTIONS'
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST','PUT','GET','OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With json auth_token X-CSRF-Token}.join(',')
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :Content_type => 'text/plain'
-    end
-  end
+  # def cors_preflight_check
+  #   if request.method == 'OPTIONS'
+  #     headers['Access-Control-Allow-Origin'] = '*'
+  #     headers['Access-Control-Allow-Methods'] = 'POST','PUT','GET','OPTIONS'
+  #     headers['Access-Control-Request-Method'] = '*'
+  #     headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With json auth_token X-CSRF-Token}.join(',')
+  #     headers['Access-Control-Max-Age'] = '1728000'
+  #     render :text => '', :Content_type => 'text/plain'
+  #   end
+  # end
 end
