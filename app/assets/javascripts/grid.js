44dc4a777164869aff5851492fff2bc85b4d616b
Joseph Won <godspeedyoo@gmail.com>
2015-04-08 23:47:49 -0700

    Add touchend listener for mobile drag feature

--- app/assets/javascripts/grid.js
+++ app/assets/javascripts/grid.js
@@ -6,30 +6,50 @@ window.onload = function() {
 		e.preventDefault();
 	});
 
+	grid.addEventListener('touchmove', function(e) {
+		e.preventDefault();
+	});
+
 	grid.addEventListener("drop", function(e) {
 		e.preventDefault();
-		var	cellId = getCellId(e);
+		var	cellIndex = getCellIndex(e);
 		if (e.target.dataset.squareState == 0) {
-			toggleX(cellId); // toggle the dropped cell only if it is unmarked
+			toggleX(cellIndex); // toggle the dropped cell only if it is unmarked
+		}
+	});
+
+	grid.addEventListener("touchend", function(e) {
+		e.preventDefault();
+		var changedTouch = e.changedTouches[0];
+		var	sourceCellIndex = getcellIndex(e);
+		var sourceCellState = e.target.dataset.squareState;
+
+		var targetCell = document.elementFromPoint(changedTouch.clientX, changedTouch.clientY); 
+		var targetCellIndex = targetCell.dataset.squareIndex;
+		var targetCellState = targetCell.dataset.squareState;
+
+	 	// toggle the dropped cell only if source is marked and target is unmarked
+		if (targetCellState == 0 && sourceCellState == 1) {
+			toggleX(targetCellIndex);
 		}
 	});
 
 	grid.addEventListener('click', function(e) {
 		e.preventDefault();
-		var	cellId = getCellId(e);
-		if (cellId == undefined) { return; } // handle error for clicking on non cell - or remove and add event listener to cells directly
-		toggleX(cellId);
+		var	cellIndex = getcellIndex(e);
+		if (cellIndex == undefined) { return; } // handle error for clicking on non cell - or remove and add event listener to cells directly
+		toggleX(cellIndex);
 	});
 
-	function getCellId(e) {
+	function getcellIndex(e) {
 		return e.target.dataset.squareIndex;
 	};
 
-	function getCellObject(cellId) {
-		return document.querySelector("[data-square-index='" + cellId + "']");
+	function getCellObject(cellIndex) {
+		return document.querySelector("[data-square-index='" + cellIndex + "']");
 	};
 
-	function toggleX(cellId) {
+	function toggleX(cellIndex) {
 		var request = new XMLHttpRequest();
 		
 		request.onreadystatechange = function() {
@@ -37,10 +57,10 @@ window.onload = function() {
 				// returns data in format of Object {data: [number]}
 
 				var data = JSON.parse(request.responseText).data;
-				var cellToUpdate = getCellObject(cellId);
+				var cellToUpdate = getCellObject(cellIndex);
 
 				if (cellToUpdate != null) {
-					if (data[cellId] === 1) {
+					if (data[cellIndex] === 1) {
 						CellModule.markCell(cellToUpdate);
 					} else {
 						CellModule.unmarkCell(cellToUpdate);
@@ -53,6 +73,6 @@ window.onload = function() {
 		request.withCredentials = true;
 		request.setRequestHeader('Content-Type',
 													   'application/x-www-form-urlencoded');
-		request.send("cellId=" + cellId);
+		request.send("cellIndex=" + cellIndex);
 	};
 }
