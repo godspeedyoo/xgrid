99244974697b62399dc01004214deecd1ff535ab
Joseph Won <godspeedyoo@gmail.com>
2015-04-09 13:28:15 -0700

    add cors preflight check method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,12 @@ class ApplicationController < ActionController::Base
     headers['Access-Control-Max-Age'] = '1728000'
   end
 
-
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+    headers['Access-Control-Allow-Origin'] = 'xgrid.herokuapp.com'
+    headers['Access-Control-Allow-Methods'] = 'POST','PUT','GET','OPTIONS'
+    headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+    headers['Access-Control-Max-Age'] = '1728000'
+    render :text => '', :Content_type => 'text/plain'
+  end
 end
