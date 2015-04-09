e5cfec85421eca74a5013b61041d7f60c0f91065
Joseph Won <godspeedyoo@gmail.com>
2015-04-09 14:54:32 -0700

    change filter to after_action in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  after_filter :set_cors
+  # protect_from_forgery with: :exception
+  after_action :set_cors
   skip_before_filter :verify_authenticity_token, :only => [:update]
   before_filter :cors_preflight_check
 
